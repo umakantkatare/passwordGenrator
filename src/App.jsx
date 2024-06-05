099fdd18572a4112486c3384c68790c5a10e9a26
Umakant Katare <umakantkatare9753121329@gmail.com>
2024-06-05 17:07:51 +0530

    update copy button

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,43 @@
 import { useCallback, useEffect, useState } from "react";
 import "./App.css";
+import { useRef } from "react";
 
 function App() {
-  const [length, setLength] = useState(6);
-  const [number, setNumber] = useState(false);
-  const [char, setChar] = useState(false);
-  const [password, setPassword] = useState("");
+  const [length, setLength] = useState("6")
+  const [number, setNumber] = useState("false")
+  const [char, setChar] = useState("false")
+  const [password, setPassword] = useState("")
 
-  const passwordGenrator = useCallback(() => {
-    let pass = "";
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const passwordRef = useRef()
 
-    if (number) str += "0123456789";
-    if (char) str += "!@#$%^&*()_+=~`";
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (number) str +=  "0123456789"
+    if (char) str += "!@#$%^&*-_+=[]{}~`"
 
     for (let index = 1; index < length; index++) {
-      let i = Math.floor(Math.random() * str.length + 1);
-      pass += str.charAt(i);
-      pass += str.charAt(pass);
-      // console.log(pass);
+      const char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char)
+      
     }
-    setPassword(pass);
-  }, [length, number, char, setPassword]);
+    setPassword(pass)
+
+  },[length, number, char, setPassword] )
+
+  const copyToClipBoard = useCallback(() => {
+   window.navigator.clipboard.writeText(password)
+   passwordRef.current?.select()
+  },[password])
 
   useEffect(() => {
-    passwordGenrator();
-  }, [length, char, number, passwordGenrator]);
+    passwordGenerator()
+  },[passwordGenerator, number, char, length])
 
   return (
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
-        <h1 className=" text-white my-2 text-center"> Password Genrator</h1>
+        <h1 className=" text-white my-2 text-center">  passwordGenerator</h1>
         <div className=" flex shadow rounded-lg overflow-hidden mb-4">
           <input
             value={password}
@@ -38,8 +45,10 @@ function App() {
             readOnly
             className="outline-none w-full py-1 px-3"
             type="text"
+            ref={passwordRef}
           />
-          <button className=" outline-none bg-blue-600 text-white px-3 py-0.5 shrink-0">
+          <button className=" outline-none bg-blue-600 text-white px-3 py-0.5 shrink-0"
+          onClick={copyToClipBoard}>
             copy
           </button>
         </div>
